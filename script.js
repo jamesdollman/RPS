8b7cf40adfa94a75589a99be4954a13448f8aad2
jamesdollman <dollmanjamie@gmail.com>
2023-10-10 15:57:07 +1100

    Fixed 5 game max

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let choice = ["rock", "paper", "scissors"]
 let playerCounter = 0;
 let computerCounter = 0;
 let tieCounter = 0;
+let rock, paper, scissors;
+let gameCounter = 0;
 
 game();
 
@@ -16,7 +18,10 @@ function getComputerChoice(number){
 }
 
 function playGame(userChoice, number, choice){
-    const computerSelection = getComputerChoice(number, choice);
+    if(gameCounter === 5 || gameCounter > 5){
+        console.log("GAME OVER >:)")
+    }else {
+        const computerSelection = getComputerChoice(number, choice);
     let result = '';
 
     if(userChoice === computerSelection){
@@ -35,46 +40,72 @@ function playGame(userChoice, number, choice){
     }
     console.log(result);
     showResults();
+    }
+
 }
 
 
 function game(){
     createElements();
+    eventListeners();
 }
 
 function createElements() {
+    //Create body selector
     const body = document.querySelector('body');
-    const rock = document.createElement('button');
-    const paper = document.createElement('button');
-    const scissors = document.createElement('button');
-    
+
+    //Create HTML Elements
+    rock = document.createElement('button');
+    paper = document.createElement('button');
+    scissors = document.createElement('button');
+    const resultsContainer = document.createElement('div');
+    const score = document.createElement('p');
+    const scoreLabel = document.createElement('label');
+
     //add text to buttons
     rock.textContent = "Rock";
     paper.textContent = "Paper";
     scissors.textContent = "Scissors";
+    scoreLabel.textContent = "Current score: ";
+
 
     //append buttons to body
     body.appendChild(rock);
     body.appendChild(paper);
     body.appendChild(scissors);
 
+    //append score, scoreLabel to resultsContainer
+    resultsContainer.appendChild(score);
+    resultsContainer.appendChild(scoreLabel);
+
+    //append resultsContainer to body
+    body.appendChild(resultsContainer);
+
+}
+
+function eventListeners() {
     //event listeners 
     rock.addEventListener('click', () => {
+        gameCounter++;
         playGame('rock', getRandomNumber(), choice);
         
     })
 
     paper.addEventListener('click', () => {
+        gameCounter++;
         playGame('paper', getRandomNumber(), choice);
     })
 
     scissors.addEventListener('click', () => {
+        gameCounter++;
         playGame('scissors', getRandomNumber(), choice);
     })
 }
 
 function showResults() {
     console.log(`Player: ${playerCounter}, Computer: ${computerCounter}, Tie: ${tieCounter}`);
+    console.log(`Games played: ${gameCounter}`);
+
 }
 
 
